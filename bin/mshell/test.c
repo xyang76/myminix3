71e1df968be814f4a33125068ce570f3ec817a6c
Xincheng Yang <xyang76@hawk.iit.edu>
2016-09-24 07:10:24 -0500

    test
    test

--- bin/mshell/test.c
+++ bin/mshell/test.c
@@ -4,11 +4,47 @@
 #include "string.h"
 #include "eval.h"
 
+char *
+padvance(char **path, char *name)
+{
+	char *p, *q;
+	char *start;
+	int len;
+
+	if (*path == NULL)
+		return NULL;
+	start = *path;
+	for (p = start ; *p && *p != ':' && *p != '%' ; p++);
+	len = p - start + strlen(name) + 2;	/* "2" is for '/' and '\0' */
+	while (stackblocksize() < len)
+		growstackblock();
+	q = stackblock();
+	if (p != start) {
+		memcpy(q, start, p - start);
+		q += p - start;
+		*q++ = '/';
+	}
+	strcpy(q, name);
+	
+	if (*p == '%') {
+		pathopt = ++p;
+		while (*p && *p != ':')  p++;
+	}
+	if (*p == ':')
+		*path = p + 1;
+	else
+		*path = NULL;
+	return stalloc(len);
+}
+
 int main(int argc, char **argv)
 {
     char * argv2[] ={"ls",0};
+    char * path1 = "/bin";
+    char * path2 = "/usr/bin";
     char *envp[]={"PATH=/bin", NULL};   
     printf("Executing...\n");
     printf("value=%d", chdir(".."));
-    evalcmd(1,argv2);
+    printf("Is exist %s...\n", padvance(&path1, "ls"));
+    printf("Is exist %s...\n", padvance(&path2, "ls"));
 }
