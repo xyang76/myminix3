77df212f5a9ae408a30a40878bc187471f187b51
Xincheng Yang <xyang76@hawk.iit.edu>
2016-09-23 22:59:24 -0500

    test

--- bin/mshell/test.c
+++ bin/mshell/test.c
@@ -5,8 +5,9 @@
 
 int main(int argc, char **argv)
 {
-    char * argv2[] ={"pwd",0};
+    char * argv2[] ={"ls",0};
+    char *envp[]={"PATH=/bin", NULL};   
     printf("Executing...\n");
     printf("value=%d", chdir(".."));
-    execvp("pwd",argv2);
+    execve("ls",argv2, envp);
 }
--- bin/mshell/test2.c
+++ bin/mshell/test2.c
@@ -5,8 +5,9 @@
 
 int main(int argc, char **argv)
 {
-    char * argv2[] ={"pwd",0};
+    char * argv2[] ={"ls",0};
+    char *envp[]={"PATH=/bin", NULL};   
     printf("Executing...\n");
-    printf("value=%d", chdir("/"));
-    execvp("pwd",argv2);
+    printf("value=%d", chdir(".."));
+    execve("/bin/ls",argv2, envp);
 }
