57ab9bf578f1d97179aac860187095101cbb680a
Xincheng Yang <xyang76@hawk.iit.edu>
2016-09-26 17:39:58 -0500

    v1.1 update

--- mshell/errdef.h
+++ mshell/errdef.h
@@ -2,11 +2,13 @@
 #define SET_ALIAS_ERR 2
 #define ALIAS_NOT_EXIST 3
 #define OPEN_PROFILE_ERR 4
-#define SET_ENV_ERR 5
-#define EMPTY_COMMAND 6
-#define COMMAND_INCORRECT 7
-#define COMMAND_EXECUTE_FAIL 8
+#define REDIRECT_HOME_FAIL 5
+#define SET_ENV_ERR 6
+#define EMPTY_COMMAND 7
+#define COMMAND_INCORRECT 8
+#define COMMAND_EXECUTE_FAIL 9
 
 void if_error(int rv, int ecode);
 void set_error(int ecode);
 void print_error(int ecode, ...);
+void TEST(int result, int expect);
