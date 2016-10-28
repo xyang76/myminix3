2e5cf04d8cf13c141b07e8d93d738ca8ea6bb7ef
Xincheng Yang <xyang76@hawk.iit.edu>
2016-10-28 15:08:03 -0500

    test

---
+++ mytest/test2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <lib.h>    // provides _syscall and message
+#include "minix/ipc.h"
+
+
+int main()
+{
+	message msg, *m;
+    int parent, child, st;
+    m = &msg;
+    st = 5;          //AMF_VALID && AMF_NOTIFY
+	parent=getpid();
+	if((child=fork())!=0){
+        //this is parent
+        msg.m1_i1 = 10;
+        printf("send %d - %d\n", child, parent);
+		i = _syscall(PM_PROC_NR, 108, &m);
+		printf("rv is %d\n", i); 
+	} else {
+        //this is child
+        printf("receive %d - %d\n", child, parent);
+		i = _syscall(PM_PROC_NR, 108, &m);
+		printf("rv is %d\n", i); 
+	}
+
+	return 0;
+}
