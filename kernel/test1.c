d01bf67352f25224b20565177bb64df311055982
Xincheng Yang <xyang76@hawk.iit.edu>
2016-10-27 19:28:31 -0500

    test

--- kernel/test1.c
+++ kernel/test1.c
@@ -2,20 +2,29 @@
 //#include <string.h>
 //#include <mprofile.h>
 //extern int load_profile(char *name);
-#include "kernel.h"
+//#include "kernel/kernel.h"
 //#include "arch_proto.h"
-#include <signal.h>
-#include <string.h>
-#include <assert.h>
-#include "proc.h"
-#include "proto.h"
-#include <machine/vm.h>
+//#include <signal.h>
+//#include <string.h>
+//#include <assert.h>
+//#include "proc.h"
+//#include "proto.h"
+//#include <machine/vm.h>
 
 
 int main()
 {
-    
-	printf("Success\n");
+	struct message msg;
+    	int parent, child;
+	
+	parent=getpid();
+	if((child=fork())==0){
+		send(child, msg);
+		printf("yes, send");
+	} else {
+		receive(parent, &msg);
+		printf("yes, receive");
+	}
 
 	return 0;
 }
