dafe83f33239229551e3bdd9c31b0fb876a790a5
Xincheng Yang <xyang76@hawk.iit.edu>
2016-10-29 01:56:20 -0500

    test

--- servers/pm/mgroup.c
+++ servers/pm/mgroup.c
@@ -1,8 +1,16 @@
 #include "stdio.h"
-#include "minix/ipc.h"
-#include <lib.h>
+#include "pm.h"
+#include <sys/wait.h>
+#include <assert.h>
+#include <minix/callnr.h>
 #include <minix/com.h>
-#include <minix/syslib.h>
+#include <minix/sched.h>
+#include <minix/vm.h>
+#include <sys/ptrace.h>
+#include <sys/resource.h>
+#include <signal.h>
+#include "mproc.h"
+#include "param.h"
 
 int grpnum=900;
 
@@ -11,8 +19,8 @@ int do_opengroup()
     message m;
     int r;
     printf("Yes, messaging\n");
-    //r = sys_ipcerrdtct(2, 10, 15);
-    r = _taskcall(SYSTASK, SYS_IPCERRDTCT, &m);
+    r = sys_ipcerrdtct(2, 10, 15);
+    //r = _taskcall(SYSTASK, SYS_IPCERRDTCT, &m);
     printf("Yes, messaging finish\n");
     return r;
 }
