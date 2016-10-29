745f7694a71644fbbab5f9da91a13cdd60ce490c
Xincheng Yang <xyang76@hawk.iit.edu>
2016-10-28 19:38:22 -0500

    test
    test

--- servers/pm/mgroup.c
+++ servers/pm/mgroup.c
@@ -1,10 +1,12 @@
 #include "stdio.h"
 #include "minix/ipc.h"
+#include "syslib.h"
 
 int grpnum=900;
 
 int do_opengroup()
 {
-    grpnum++;
-    return grpnum;
+    message m;
+
+    return(_kernel_call(SYS_IPCERRDTCT, &m));
 }
