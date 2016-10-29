fda78348fceadaaf0789602755fdde29fd49bfb4
Xincheng Yang <xyang76@hawk.iit.edu>
2016-10-29 18:11:41 -0500

    test

--- mytest/testhelper.h
+++ mytest/testhelper.h
@@ -19,7 +19,7 @@ static inline void TEST_EQUAL(int result, int expect, char *msg){
         printf("TEST [%s] OK.\n", msg);
     } else {
         printf("TEST [%s] Error, expect %d, result %d\n", msg, expect, result);
-        abort();
+        exit(0);
     }
 }
 
@@ -28,6 +28,6 @@ static inline void TEST_GREATER(int val1, int val2, char *msg){
         printf("TEST [%s] OK.\n", msg);
     } else {
         printf("TEST [%s] Error, value1 %d, value2 %d\n", msg, val1, val2);
-        abort();
+        exit(0);
     }
 }
