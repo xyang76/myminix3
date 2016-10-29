28e972c23eb690fdadb1e58fa3307cf0e20d5a03
Xincheng Yang <xyang76@hawk.iit.edu>
2016-10-29 17:07:51 -0500

    test

---
+++ mytest/testgroup.c
@@ -0,0 +1,32 @@
+#include <lib.h>    
+#include <stdio.h>
+#include <uinst.h>
+#include <testhelper.h>
+
+/* test cases */
+int test_opengroup_syscall();
+int test_opengroup();
+
+int main(void) {
+    test_opengroup_syscall();
+    test_opengroup();
+}
+
+int test_opengroup_syscall(){
+    message m;  
+    int i;
+
+    m.m1_i1 = 0;
+    
+    /* group id start from 0 */
+    TEST_GREATER(_syscall(PM_PROC_NR, OPENGP, &m), -1, "opengroup should return id bigger than -1");
+}
+
+int test_opengroup(){
+    int gid1, gid2;
+    
+    gid1 = opengroup(0);
+    gid2 = opengroup(0);
+    TEST_GREATER(gid1, -1, "opengroup should return id bigger than -1");        
+    TEST_GREATER(gid2, gid1, "new group id > old group id");     
+}
---
+++ mytest/testhelper.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define TRUE 1
+#define FALSE 0
+
+static inline int ASSERT_EQUAL(int result, int expect)
+{
+    if(result == expect){
+        return TRUE;
+    }
+    return FALSE;
+}
+
+static inline void TEST_EQUAL(int result, int expect, char *msg){
+    if(result == expect){
+        printf("TEST [%s] OK.\n", msg);
+    } else {
+        printf("TEST [%s] Error, expect %d, result %d\n", msg, expect, result);
+        abort();
+    }
+}
+
+static inline void TEST_GREATER(int val1, int val2, char *msg){
+    if(val1 > val2){
+        printf("TEST [%s] OK.\n", msg);
+    } else {
+        printf("TEST [%s] Error, value1 %d, value2 %d\n", msg, val1, val2);
+        abort();
+    }
+}
