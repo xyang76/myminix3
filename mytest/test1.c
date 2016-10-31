ca7125359ceedbe5bbdfc03f68e9ddccbaf7e083
Xincheng Yang <xyang76@hawk.iit.edu>
2016-10-31 18:21:44 -0500

    test

--- mytest/test1.c
+++ mytest/test1.c
@@ -15,16 +15,17 @@ int main()
 		msg.m1_i1 = 10;
 		printf("send %d - %d\n", child, parent);
 		rv=send(child, m);
-		printf("yes, child send success, rv=%d\n", rv);
+		printf("yes, parent send success, rv=%d\n", rv);
+        rv=receive(child, &msg, &st);
+        printf("yes, parent rec success, rv=%d\n", rv);
 	} else {
 		//this is child
 		printf("receive %d - %d\n", child, parent);
 		//while(1){
         rv = receive(parent, &msg, &st);
-		//	if(rv == 0){
-        printf("yes, parent receive success %d, rv=%d\n", msg.m1_i1, rv);
-		//	}
-		//}
+        printf("yes, child receive success %d, rv=%d\n", msg.m1_i1, rv);
+		rv=send(parent, m);
+        printf("yes, child send success, rv=%d\n", rv);
 	}
 	while(1);		//infinity loop to keep this proc not terminate.
 	return 0;
