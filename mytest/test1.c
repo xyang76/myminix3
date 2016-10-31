fa7378b5a5a5622abca51f8eda80c49bd757b554
Xincheng Yang <xyang76@hawk.iit.edu>
2016-10-31 16:47:01 -0500

    test

--- mytest/test1.c
+++ mytest/test1.c
@@ -6,20 +6,20 @@
 int main()
 {
 	message msg, *m;
-    int parent, child, st;
-    m = &msg;
-    st = 5;          //AMF_VALID && AMF_NOTIFY
+	int parent, child, st;
+	m = &msg;
+	st = 5;          //AMF_VALID && AMF_NOTIFY
 	parent=getpid();
 	if((child=fork())!=0){
-        //this is parent
-        msg.m1_i1 = 10;
-        printf("send %d - %d\n", child, parent);
+		//this is parent
+		msg.m1_i1 = 10;
+		printf("send %d - %d\n", child, parent);
 		send(child, m);
 		printf("yes, child send success\n");
 	} else {
-        //this is child
-        printf("receive %d - %d\n", child, parent);
-		receive(0, &msg, &st);
+		//this is child
+		printf("receive %d - %d\n", child, parent);
+		//receive(0, &msg, &st);
 		printf("yes, parent receive success\n");
 	}
 
