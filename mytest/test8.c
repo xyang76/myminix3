8c9088b4570f65ec546064991acd469d7a156f36
Xincheng Yang <xyang76@hawk.iit.edu>
2016-10-31 19:15:25 -0500

    test

--- mytest/test8.c
+++ mytest/test8.c
@@ -27,6 +27,7 @@ int main()
 	}*/
     message msg;
     int status,i, pid[10], st=5, rv;
+    int parent = getpid();
     for (i = 0; i < 10; i++){
         status = fork();
         if (status == 0 || status == -1) break;
@@ -35,20 +36,20 @@ int main()
     if (status == -1){
         //Fork error
     } else if (status == 0){
-        if(i<1){        //Receiver
-            while(1){
-                if(receive(pid[0], &msg, &st)){
+//        if(i<1){        //Receiver
+            
+//        } else {
+        msg.m1_i1 = 10;
+        rv = send(parent, &msg);
+        printf("send %d %d", parent, rv);
+//        }
+    } else {
+        while(1){
+            for(i=0; i<10; i++){
+                if(receive(pid[i], &msg, &st)){
                     printf("Yes rec success! %d\n", msg.m1_i1);
                 }
             }
-        } else {
-            msg.m1_i1 = 10;
-            rv = send(pid[0], &msg);
-            printf("send %d %d", pid[0], rv);
-        }
-    } else {
-        for(i=0; i<10; i++){
-            printf("pid=%d\n", pid[i]);
         }
     }
 	return 0;
