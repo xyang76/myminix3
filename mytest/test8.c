fd857e985227b8bd8bac4cc45b9a8fe8cb24772a
Xincheng Yang <xyang76@hawk.iit.edu>
2016-11-01 20:39:12 -0500

    test

--- mytest/test8.c
+++ mytest/test8.c
@@ -39,29 +39,6 @@ int main()
 {
     message msg;
     int status,i, pid[10], st=5, rv;
-    int server;
-    if((server=fork()) == 0){
-        message m, *m_ptr;
-        int result;                 
-
-      /* SEF local startup. */
-//      env_setargs(argc, argv);
-        sef_setcb_init_fresh(sef_cb_init_fresh);
-  sef_setcb_init_restart(sef_cb_init_fail);
-
-  /* No live update support for now. */
-
-  /* Let SEF perform startup. */
-        sef_startup();
-      m_ptr = &m;
-      while (TRUE) { 
-          
-        int status = sef_receive(0, m_ptr); 
-        printf("message %d type %d", m_ptr->m_source, m_ptr->m_type);
-        send(m_ptr->m_source, m_ptr);
-      }
-      exit(0);
-    }
     
     for (i = 0; i < 10; i++){
         status = fork();
@@ -71,21 +48,17 @@ int main()
     if (status == -1){
         //Fork error
     } else if (status == 0){
-        msg.m1_i1 = 10;
-        msg.m_source = getpid();
-        msg.m_type = 100;
-        rv = send(server, &msg);
-        printf("send %d %d\n", server, rv);
+        //Child proc
     } else {
-        
-//        printf("cur id:%d\n", parent);
-//        while(1){
-//            for(i=0; i<10; i++){
-//                if(receive(pid[i], &msg, &st) == 0){
-//                    printf("Yes rec success! %d\n", msg.m1_i1);
-//                }
-//            }
-//        }
+        //Parent proc    
+        printf("cur id:%d\n", parent);
+        while(1){
+            for(i=0; i<10; i++){
+                if(receive(pid[i], &msg, &st) == 0){
+                    printf("Yes rec success! %d\n", msg.m1_i1);
+                }
+            }
+        }
     }
 	return 0;
 }
