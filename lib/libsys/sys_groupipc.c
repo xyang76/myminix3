79edfa6033dcf5c29909fcd85f5637058862d66a
Xincheng Yang <xyang76@hawk.iit.edu>
2016-11-02 00:39:39 -0500

    test

--- lib/libsys/sys_groupipc.c
+++ lib/libsys/sys_groupipc.c
@@ -13,7 +13,19 @@ int sys_ipcerrdtct(int function, endpoint_t src_e, endpoint_t dest_e)
     return (_kernel_call(SYS_IPCERRDTCT, &m));
 }
 
-int sys_ipcerrdtct(int function, endpoint_t src_e, endpoint_t dest_e)
+int sys_groupsend(int function, endpoint_t src_e, endpoint_t dest_e)
+{
+	int r;
+    message m, msg;
+    
+    m.m1_i1 = dest_e;
+    m.m1_p1 = msg;
+    m.m_type = ((message*)msg).m_type;
+    m.m_source = src_e;
+    return (_kernel_call(SYS_IPCERRDTCT, &m));
+}
+
+int sys_groupreceive(int function, endpoint_t src_e, endpoint_t dest_e)
 {
 	int r;
     message m, msg;
