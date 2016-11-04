32870ece06511675674c85fc8631f44e221b7ed6
Xincheng Yang <xyang76@hawk.iit.edu>
2016-11-03 23:33:39 -0500

    t

--- servers/pm/mgroup.c
+++ servers/pm/mgroup.c
@@ -308,9 +308,9 @@ void unblock(endpoint_t proc_e, message *msg){
             }
             setreply(proc_nr, OK);
             if ((rmp->mp_flags & (REPLY | IN_USE | EXITING)) == (REPLY | IN_USE)) {
-//              s=sendnb(rmp->mp_endpoint, &rmp->mp_reply);
               memcpy(&rmp->mp_reply, msg, sizeof(message));
-              s=sendnb(rmp->mp_endpoint, msg);
+              printf("ub_send %d-%d", msg.m1_i1, rmp->mp_reply.m1_i1);
+              s=sendnb(rmp->mp_endpoint, &rmp->mp_reply);
               if (s != OK) {
                   printf("PM can't reply to %d (%s): %d\n",
                       rmp->mp_endpoint, rmp->mp_name, s);
