1e9bedfd003addda6f8da1fad991704394097702
Xincheng Yang <xyang76@hawk.iit.edu>
2016-11-04 21:20:17 -0500

    t

---
+++ mytest/testipc_singleipc.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <lib.h>    
+#include <testhelper.h>
+#include "minix/ipc.h"
+
+int main()
+{
+    int gid, rv, child, parent=getpid();
+    message m1, m2;
+    
+    gid=opengroup(0);
+    ASSERT_GREATER(gid, 0);
+    
+    if((child=fork())==0){
+        // child
+        printf("Child start send %d\n", child);
+        rv = addproc(gid, getpid());
+        ASSERT_EQUAL(rv, 0);
+        
+        m1.m1_i1 = 10;
+        rv = msend(gid, &m1, parent);
+        
+        TEST_EQUAL(rv, 0, "msend finish should return 0");
+    } else {
+        // This is parent
+        printf("Parent start receive %d\n", parent);
+        rv = addproc(gid, parent);
+        ASSERT_EQUAL(rv, 0);
+        
+        rv = mreceive(gid, &m2, child);
+        TEST_EQUAL(rv, 0, "mreceive finish should return 0");
+        TEST_EQUAL(m2.m1_i1, 10, "receive value should be 10");
+    }
+	return 0;
+}
