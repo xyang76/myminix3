db551a700938f5988fc1e72da57f7f0bfb2a6334
Xincheng Yang <xyang76@hawk.iit.edu>
2016-11-05 23:34:54 -0500

    t

--- mytest/testipcorder.c
+++ mytest/testipcorder.c
@@ -10,7 +10,7 @@
  *************************************************************/
 int main()
 {
-    message m1, m2, m3, m;
+    message m1, m2, m3;
     int status,i, pid[10], rv, parent=getpid(), value[3];
     int gid = opengroup(0);
     ASSERT_GREATER(gid, 0);
@@ -29,29 +29,30 @@ int main()
         //Fork error
     } else if (status == 0){
         //Child proc
+        message m;
         rv = mreceive(gid, &m, parent);    	
-	ASSERT_EQUAL(rv, 0);
+        ASSERT_EQUAL(rv, 0);
         TEST_EQUAL(m.m1_i1, 10, "message should be 10.");
-	rv = mreceive(gid, &m, parent);    	
-	ASSERT_EQUAL(rv, 0);
+        rv = mreceive(gid, &m, parent);    	
+        ASSERT_EQUAL(rv, 0);
         TEST_EQUAL(m.m1_i1, 20, "message should be 20.");
-	rv = mreceive(gid, &m, parent);
-	ASSERT_EQUAL(rv, 0);    	
+        rv = mreceive(gid, &m, parent);
+        ASSERT_EQUAL(rv, 0);    	
         TEST_EQUAL(m.m1_i1, 30, "message should be 30.");	
     } else {
         //Parent proc   
-	m1.m1_i1 = 10;
-	rv = msend(gid, &m1, SENDALL);
-	ASSERT_EQUAL(rv, 0);
-	TEST_EQUAL(rv, 0, "msend finish should return 0");
-	m2.m1_i1 = 20;
-	rv = msend(gid, &m2, SENDALL);
-	ASSERT_EQUAL(rv, 0);
-	TEST_EQUAL(rv, 0, "msend finish should return 0");
-	m3.m1_i1 = 30;
-	rv = msend(gid, &m3, SENDALL);
-	ASSERT_EQUAL(rv, 0);
-	TEST_EQUAL(rv, 0, "msend finish should return 0");
+        m1.m1_i1 = 10;
+        rv = msend(gid, &m1, SENDALL);
+        ASSERT_EQUAL(rv, 0);
+        TEST_EQUAL(rv, 0, "msend finish should return 0");
+        m2.m1_i1 = 20;
+        rv = msend(gid, &m2, SENDALL);
+        ASSERT_EQUAL(rv, 0);
+        TEST_EQUAL(rv, 0, "msend finish should return 0");
+        m3.m1_i1 = 30;
+        rv = msend(gid, &m3, SENDALL);
+        ASSERT_EQUAL(rv, 0);
+        TEST_EQUAL(rv, 0, "msend finish should return 0");
         closegroup(gid);
     }
     return 0;
