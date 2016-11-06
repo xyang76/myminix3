f0984931c476e5a19aa3aae8172f3fb68d3f9ea0
Xincheng Yang <xyang76@hawk.iit.edu>
2016-11-06 01:46:00 -0500

    t
    t

--- servers/pm/mgroup.c
+++ servers/pm/mgroup.c
@@ -382,10 +382,10 @@ void try_unblock(mqueue *block_queue, mqueue *unblock_queue, int call_type){
             case RECEIVE:
                 do_unblock(g_m->receiver, g_m->msg);        // unblock receiver
                 printf("branch 2 :: ");
-                if(getprocqueue(g_m->sender, &proc_q) > 0){ // unblock sender
+                if(getprocqueue(g_m->sender, &proc_q) > -1){ // unblock sender
                     for(n = proc_q->head; n != NULL; n=n->nextNode){
                         msg_m = (grp_message *)n->value;
-                        if(msg_m->call_nr == RECEIVE  || msg_m->receiver == g_m->receiver) continue;
+                        if(msg_m->call_nr == RECEIVE) continue;
                         send_num++;
                     }
                     printf("branch[%d] 3 :: ", send_num);
