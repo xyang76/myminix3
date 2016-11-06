56a0d99f129d1c9004a141d7df2979ba399eb1b7
Xincheng Yang <xyang76@hawk.iit.edu>
2016-11-05 22:53:55 -0500

    t
    t

--- mytest/testmultiplegroup.c
+++ mytest/testmultiplegroup.c
@@ -10,49 +10,54 @@
  *************************************************************/
 int main()
 {
-    message m1, m2, m3, m;
+    message m1, m2, m3;
     int status,i, pid[10], rv, parent=getpid(), value[3];
-    int gid = opengroup(0);
-    ASSERT_GREATER(gid, 0);
+    int gid1 = opengroup(0);
+    int gid2 = opengroup(0);
+    ASSERT_GREATER(gid1, 0);
+    ASSERT_GREATER(gid2, gid1);
     
-    rv = addproc(gid, parent);
+    rv = addproc(gid1, parent);
+    ASSERT_EQUAL(rv, 0);
+    rv = addproc(gid2, parent);
     ASSERT_EQUAL(rv, 0);
 
-    for (i = 0; i < 3; i++){
+    for (i = 0; i < 10; i++){
         status = fork();
         if (status == 0 || status == -1) break;
         pid[i] = status;
-        rv = addproc(gid, pid[i]);
+	if(i<4){
+            rv = addproc(gid1, pid[i]);
+	} else {
+	    rv = addproc(gid2, pid[i]);
+	}
 	ASSERT_EQUAL(rv, 0);
     }
     if (status == -1){
         //Fork error
     } else if (status == 0){
         //Child proc
-        rv = mreceive(gid, &m, parent);    	
-	ASSERT_EQUAL(rv, 0);
-        TEST_EQUAL(m.m1_i1, 10, "message should be 10.");
-	rv = mreceive(gid, &m, parent);    	
-	ASSERT_EQUAL(rv, 0);
-        TEST_EQUAL(m.m1_i1, 20, "message should be 20.");
-	rv = mreceive(gid, &m, parent);
-	ASSERT_EQUAL(rv, 0);    	
-        TEST_EQUAL(m.m1_i1, 30, "message should be 20.");	
+	if(i<4){
+            rv = mreceive(gid1, &m2, parent);   
+	    ASSERT_EQUAL(rv, 0);
+            TEST_EQUAL(m2.m1_i1, 10, "message should be 10.");
+	} else {
+            rv = mreceive(gid1, &m3, parent);   
+	    ASSERT_EQUAL(rv, 0);
+            TEST_EQUAL(m3.m1_i1, 20, "message should be 20.");
+	} 	
+	
     } else {
         //Parent proc   
 	m1.m1_i1 = 10;
-	rv = msend(gid, &m1, SENDALL);
+	rv = msend(gid1, &m1, SENDALL);
 	ASSERT_EQUAL(rv, 0);
 	TEST_EQUAL(rv, 0, "msend finish should return 0");
-	m2.m1_i1 = 20;
-	rv = msend(gid, &m2, SENDALL);
+
+	m1.m1_i1 = 20;
+	rv = msend(gid2, &m1, SENDALL);
 	ASSERT_EQUAL(rv, 0);
 	TEST_EQUAL(rv, 0, "msend finish should return 0");
-	m3.m1_i1 = 30;
-	rv = msend(gid, &m3, SENDALL);
-	ASSERT_EQUAL(rv, 0);
-	TEST_EQUAL(rv, 0, "msend finish should return 0");
-        closegroup(gid);
     }
     return 0;
 }
