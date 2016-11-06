4004653086dee6bfee115a28edd38864fd209f6e
Xincheng Yang <xyang76@hawk.iit.edu>
2016-11-05 19:19:24 -0500

    t

--- mytest/testhelper.h
+++ mytest/testhelper.h
@@ -11,7 +11,7 @@ static inline int ASSERT_EQUAL(int result, int expect)
     if(result == expect){
         return TRUE;
     }
-    printf("ASSERT_EQUAL [%d==%d] Fail.\n", result, expect);
+    printf("ASSERT_EQUAL [%d==%d] line %d Fail.\n", result, expect, __LINE__);
     exit(0);
 }
 
@@ -20,7 +20,7 @@ static inline int ASSERT_GREATER(int result, int expect)
     if(result > expect){
         return TRUE;
     }
-    printf("ASSERT_EQUAL [%d==%d] Fail.\n", result, expect);
+    printf("ASSERT_EQUAL [%d==%d] line %d Fail.\n", result, expect, __LINE__);
     exit(0);
 }
 
@@ -28,7 +28,7 @@ static inline void TEST_EQUAL(int result, int expect, char *msg){
     if(result == expect){
         printf("TEST_EQUAL [%s] OK.\n", msg);
     } else {
-        printf("TEST_EQUAL [%s] Error, expect %d, result %d, errno %d\n", msg, expect, result, errno);
+        printf("TEST_EQUAL [%s] Error, line %d, expect %d, result %d, errno %d\n", msg, __LINE__, expect, result, errno);
         exit(0);
     }
 }
@@ -37,7 +37,7 @@ static inline void TEST_GREATER(int val1, int val2, char *msg){
     if(val1 > val2){
         printf("TEST_GREATER [%s] OK.\n", msg);
     } else {
-        printf("TEST_GREATER [%s] Error, value1 %d, value2 %d, errno %d\n", msg, val1, val2, errno);
+        printf("TEST_GREATER [%s] Error, line %d, value1 %d, value2 %d, errno %d\n", msg, __LINE__, val1, val2, errno);
         exit(0);
     }
 }
