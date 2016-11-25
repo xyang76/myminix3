517e3451183470e6c6551376240dbaa3d9d6caa8
Xincheng Yang <xyang76@hawk.iit.edu>
2016-11-24 18:00:08 -0600

    t
    t

--- lib/libc/sys-minix/undelete.c
+++ lib/libc/sys-minix/undelete.c
@@ -8,13 +8,23 @@
 __weak_alias(unlink, _unlink)
 #endif
 
-int fundelete(name)
-const char *name;
+int fundelete(file)
+const char *file;
 {
-  message m;
+    struct stat sb;
+    message m;
+    
+    _DIAGASSERT(file != NULL);
 
-  _loadname(name, &m);
-  return(_syscall(VFS_PROC_NR, UNDELETE, &m));
+    if (lstat(file, &sb) < 0)
+        return -1;
+
+    if (S_ISDIR(sb.st_mode))
+        m.m3_i2 = 1;
+    else
+        m.m3_i2 = 2;
+    _loadname(name, &m);
+    return(_syscall(VFS_PROC_NR, UNDELETE, &m));
 }
 
 int rcmkdir(const char *name, mode_t mode)
